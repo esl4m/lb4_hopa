5f6e59094942ac1bfb0167765c13d5f24587f78c
esl4m <me@esl4m.com>
2019-04-05 16:36:28 +0300

    Continue the basic authentcation ToDo // Adds better method for checking & comparing the password

--- src/providers/auth-strategy.provider.ts
+++ src/providers/auth-strategy.provider.ts
@@ -1,3 +1,4 @@
+import { repository } from '@loopback/repository';
 import { Provider, inject, ValueOrPromise } from '@loopback/context';
 import { Strategy } from 'passport';
 import {
@@ -6,9 +7,15 @@ import {
   UserProfile,
 } from '@loopback/authentication';
 import { BasicStrategy } from 'passport-http';
+import { UserRepository } from '../repositories';
+
+const md5 = require('md5');
 
 export class MyAuthStrategyProvider implements Provider<Strategy | undefined> {
   constructor(
+    @repository(UserRepository)
+    public userRepository: UserRepository,
+
     @inject(AuthenticationBindings.METADATA)
     private metadata: AuthenticationMetadata,
   ) { }
@@ -21,19 +28,28 @@ export class MyAuthStrategyProvider implements Provider<Strategy | undefined> {
 
     const name = this.metadata.strategy;
     if (name === 'BasicStrategy') {
-      return new BasicStrategy(this.verify);
+      return new BasicStrategy(this.verify.bind(this));
     } else {
       return Promise.reject(`The strategy ${name} is not available.`);
     }
   }
 
-  verify(
+  async verify(
     username: string,
     password: string,
-    cb: (err: Error | null, user?: UserProfile | false) => void,
+    cb: (err: Error | null, user?: object | false) => void,  // replace UserProfile with object "datatype" for user
   ) {
+    const user = await this.userRepository.findOne({ where: { username: username } });
     // find user by name & password
-    // call cb(null, false) when user not found
-    // call cb(null, user) when user is authenticated
+    if (user) {
+      console.log(password);
+
+      if (md5(password) == user.password) {
+
+        cb(null, user);  // when user is authenticated
+      }
+    } else {
+      cb(null, false);  // when user not found
+    }
   }
 }
